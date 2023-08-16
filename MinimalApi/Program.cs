1ad44acae1f915b125c93d8f381f8150f93dd879
coffinslugs <55113474+coffinslugs@users.noreply.github.com>
2023-08-16 06:49:56 -0600

    Add project files.

---
+++ MinimalApi/Program.cs
@@ -0,0 +1,21 @@
+using System.Reflection.Metadata.Ecma335;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/api/Todo", () => new string[] { "string1", "string2" });
+app.MapGet("/api/Todo/{id}", (int id) => $"Id: {id}");
+
+app.Run();
